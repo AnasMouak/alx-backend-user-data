57bc57522407b745ed76aaf50a317c008257dfdf
AnasMouak <anasmouak1111@gmail.com>
2024-08-28 17:19:11 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,8 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """returns the log message obfuscated"""
     pattern = f"({'|'.join(fields)})=[^{separator}]+"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
