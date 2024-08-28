3e87d36dd6567b1d38a1ee4562f32349cbceeeb8
AnasMouak <anasmouak1111@gmail.com>
2024-08-28 17:17:42 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Regex-ing"""
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     pattern = f"({'|'.join(fields)})=[^{separator}]+"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
