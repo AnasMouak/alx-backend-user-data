e2882c7936e960d15012d21e5dee8d5066d422b0
AnasMouak <anasmouak1111@gmail.com>
2024-09-04 22:35:15 +0000

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -52,3 +52,28 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """
+        Extracts the user email and password from the decoded Base64
+        Authorization header.
+
+        Args:
+            decoded_base64_authorization_header (str): The decoded Base64
+            string.
+
+        Returns:
+            tuple: The user email and password, or (None, None) if conditions
+            are not met.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
