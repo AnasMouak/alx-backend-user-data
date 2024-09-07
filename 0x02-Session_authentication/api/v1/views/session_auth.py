5276d60aaeaa017b136fdfed509194b2ba5f4e6b
AnasMouak <anasmouak1111@gmail.com>
2024-09-07 20:33:46 +0000

    Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -38,3 +38,13 @@ def auth_session_login():
     response.set_cookie(session_name, session_id)
     
     return response
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def auth_session_logout():
+    """DELETE /auth_session/logout to handle user logout"""
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
